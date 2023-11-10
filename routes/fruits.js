d1ee05e9d15c612d683df4b5090aabb4f542085d
Maxim Toma <maxim.toma@sky.uk>
2023-11-10 13:32:43 +0000

    Created all GET, PUT, POST, DELETE (as well as /:id) for /fruits endpoint in router file

--- routes/fruits.js
+++ routes/fruits.js
@@ -2,4 +2,41 @@ const express = require("express");
 const router = express.Router();
 const { Fruit } = require("../models/index");
 
+router
+  .route("/")
+  .get(async (req, res) => {
+    const allOfThem = await Fruit.findAll();
+    res.json(allOfThem);
+  })
+  .post(async (req, res) => {
+    const theData = req.body;
+    await Fruit.create(theData);
+    const allOfThem = await Fruit.findAll();
+
+    res.json(allOfThem);
+  });
+
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const theId = req.params.id;
+    const theFruit = await Fruit.findByPk(theId);
+    res.json(theFruit);
+  })
+  .put(async (req, res) => {
+    const theId = req.params.id;
+    const theData = req.body;
+    await Fruit.update(theData, { where: { id: theId } });
+    const allOfThem = await Fruit.findAll();
+
+    res.json(allOfThem);
+  })
+  .delete(async (req, res) => {
+    const theId = req.params.id;
+    await Fruit.destroy({ where: { id: theId } });
+    const allOfThem = await Fruit.findAll();
+
+    res.json(allOfThem);
+  });
+
 module.exports = router;
