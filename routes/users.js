3467bf22bcaea17bb23183bbaacca9afdec7c4f2
Maxim Toma <maxim.toma@sky.uk>
2023-11-10 12:16:34 +0000

    Created GET by id and POST endopints for /users

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,22 @@ const { User } = require("../models/index");
 
 router.get("/", async (req, res) => {
   const theRes = await User.findAll();
+  res.json(theRes);
+});
+
+router.get("/:id", async (req, res) => {
+  const theId = req.params.id;
+  const theUser = await User.findByPj(theId);
+
+  res.json(theRes);
+});
+
+router.post("/", async (req, res) => {
+  const theData = req.body;
+  await User.create(theData);
+  const allOfThem = await User.findAll();
+
+  res.json(allOfThem);
 });
 
 module.exports = router;
